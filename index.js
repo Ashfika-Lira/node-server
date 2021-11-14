f12e8c559d2d60618b39ce6e2aa29ab1bd3a1b80
unknown <ashleyalam33@gmail.com>
2021-11-14 15:22:15 +0600

    json

--- index.js
+++ index.js
@@ -8,33 +8,33 @@ const port = process.env.PORT || 8000;
 app.use(cors());
 app.use(express.json());
 
-// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hwgc0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hwgc0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+console.log(uri)
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-// async function run() {
-//     try {
-//         await client.connect();
-//         const database = client.db('watch');
-//         const watchCollection = database.collection('products');
-        // const ordersCollection = database.collection('orders');
-        // const reviewsCollection = database.collection('reviews');
-        // const usersCollection = database.collection('users');
+ async function run() {
+     try {
+        await client.connect();
+       const database = client.db('watch');
+       const watchCollection = database.collection('products');
+         const ordersCollection = database.collection('orders');
+         const reviewsCollection = database.collection('reviews');
+         const usersCollection = database.collection('users');
 
-			// 	//post a review
-	// 	app.post('/reviews', async (req, res) => {
-	// 		const review = req.body;         
-    //         const result = await reviewCollection.insertOne(review);
-    //         res.json(result);
+		// 		//post a review
+		// app.post('/reviews', async (req, res) => {
+		// 	const review = req.body;         
+        //     const result = await reviewCollection.insertOne(review);
+        //     res.json(result);
         
-    //     });
+        // });
 
-	// 	//post method for get orders by ID
-    //     app.post('/orders', async (req, res) => {
-    //         const order = req.body;
-    //         const result = await ordersCollection.insertOne(order);
-    //         res.json(result);
-    //     })
+		//post method for get orders by ID
+        // app.post('/orders', async (req, res) => {
+        //     const order = req.body;
+        //     const result = await ordersCollection.insertOne(order);
+        //     res.json(result);
+        // })
 
     //     // UPDATE ORDER OR INSERT ORDER
     //     app.put('/users/:email', async (req, res) => {
@@ -65,11 +65,11 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 	// 	});
         
        // get method
-	//    app.get('/products', async (req, res) => {
-	// 	const cursor = watchCollection.find({});
-	// 	const allWatches = await cursor.toArray();
-	// 	res.send(allWatches);
-	// });
+	   app.get('/products', async (req, res) => {
+		const cursor = ordersCollection.find({});
+		const allWatches = await cursor.toArray();
+		res.send(allWatches);
+	});
 //     app.get('/reviews', async (req, res) => {
 //         const cursor = reviewCollection.find({});
 //         const allReviews = await cursor.toArray();
@@ -91,19 +91,19 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 //         res.json(result);
 //     })
 
+console.log('connected to db_ref')
+	}
+    finally {
+        // await client.close();
+    }
+}
 
-// 	}
-//     finally {
-//         // await client.close();
-//     }
-// }
+ run().catch(console.dir);
 
-// run().catch(console.dir);
+app.get('/', (req, res) => {
+    res.send('Running Watch Server');
+});
 
-// app.get('/', (req, res) => {
-//     res.send('Running Watch Server');
-// });
-
-// app.listen(port, () => {
-//     console.log('Running Watch Server on port:', port);
-// })
+app.listen(port, () => {
+    console.log('Running Watch Server on port:', port);
+})
